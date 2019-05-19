09781f5bf9301335c2e542cf207e334610540a5c
vahid chakoshy <vchakoshy@gmail.com>
2019-05-19 14:39:00 +0430

    fix cors

--- api/api.go
+++ api/api.go
@@ -1,9 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"log"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	"github.com/volatiletech/sqlboiler/boil"
@@ -27,7 +27,8 @@ func Run() {
 	boil.SetDB(db)
 
 	r := gin.Default()
-	r.Use(cors.Default())
+
+	r.Use(corsMiddleware())
 
 	v1 := r.Group("/api/v1/")
 	{
@@ -43,4 +44,22 @@ func Run() {
 
 	err = r.Run("0.0.0.0:8080")
 	log.Println(err.Error())
+}
+
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
 }
