f9999385c317615260def66d9b890a03828dc445
vahid chakoshy <vchakoshy@gmail.com>
2019-05-21 15:50:24 +0430

    add best seller

--- api/rest/flex_setup.go
+++ api/rest/flex_setup.go
@@ -47,6 +47,12 @@ func (s Setup) getSort() []qm.QueryMod {
 	switch s.Sort.Value {
 	case "RECENT":
 		q = append(q, qm.OrderBy("id DESC"))
+	case "BESTSELLER":
+		// ->innerJoin("BookStats ON BookStats.book_id=Book.id")
+		// ->orderBy("BookStats.all_sales_count DESC");
+		q = append(q, qm.InnerJoin("book_stats ON book_stats.book_id=book.id"))
+		q = append(q, qm.OrderBy("book_stats.all_sales_count DESC"))
+
 	}
 
 	return q
