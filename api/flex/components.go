7740d6641952998d91c750d60499d86469b1d2b8
Ali Irani <irani.2007@gmail.com>
2019-05-26 16:39:29 +0430

    Add hl_books_library component

--- api/flex/components.go
+++ api/flex/components.go
@@ -83,3 +83,15 @@ func handleListComponent(cs flexComponentSettings, t string) (com flexComponent)
 	}
 	return
 }
+
+func handleBooksLibraryComponent(cs flexComponentSettings) (com flexComponent) {
+	m := make(map[string]string)
+	m["format"] = cs.Settings.Format
+	m["content_type"] = cs.Settings.ContentType
+	com.Data.Items.Generic = append(com.Data.Items.Generic, m)
+	com.Title = cs.Elements.Title.Value.Static
+	com.Icon = cs.Elements.Icon.Value
+	com.Type = "HL_BOOKS_LIBRARY"
+	com.ResourceType = "CUSTOM"
+	return
+}
