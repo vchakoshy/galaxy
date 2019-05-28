0cf583d5c71b4fb9d9f71c47967db9b7f4a5509e
vahid chakoshy <vchakoshy@gmail.com>
2019-05-28 14:01:04 +0430

    clean code and remove unused parts

--- api/flex/setup.go
+++ api/flex/setup.go
@@ -2,8 +2,6 @@ package flex
 
 import (
 	"strings"
-
-	"github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 type Setup struct {
@@ -36,20 +34,6 @@ type Setup struct {
 	} `json:"format"`
 }
 
-func (s Setup) getQueries() []qm.QueryMod {
-	q := make([]qm.QueryMod, 0)
-
-	q = append(q, s.Book.getQuery()...)
-	q = append(q, s.Author.getQuery()...)
-	q = append(q, s.Category.getQuery()...)
-	q = append(q, s.Channel.getQuery()...)
-	q = append(q, s.Publisher.getQuery()...)
-	q = append(q, s.ProposedList.getQuery()...)
-	q = append(q, s.Tag.getQuery()...)
-
-	return q
-}
-
 func (s Setup) getInputActions() []ComponentAction {
 	q := make([]ComponentAction, 0)
 
@@ -65,34 +49,6 @@ func (s Setup) getInputActions() []ComponentAction {
 		if ca.Key != "" {
 			q = append(q, ca)
 		}
-	}
-
-	return q
-}
-
-func (s Setup) getSort() []qm.QueryMod {
-	q := make([]qm.QueryMod, 0)
-
-	switch s.Sort.Value {
-	case "RECENT":
-		q = append(q, qm.OrderBy("id DESC"))
-	case "BESTSELLER":
-		q = append(q, qm.InnerJoin("book_stats ON book_stats.book_id=book.id"))
-		q = append(q, qm.OrderBy("book_stats.all_sales_count DESC"))
-	case "WEEK_BESTSELLER":
-		q = append(q, qm.InnerJoin("book_stats ON book_stats.book_id=book.id"))
-		q = append(q, qm.OrderBy("book_stats.week_sales_count DESC"))
-	case "RECENT_PUBLISH":
-		q = append(q, qm.OrderBy("book.publish_date DESC"))
-	case "POPULAR":
-		q = append(q, qm.InnerJoin("book_stats ON book_stats.book_id=book.id"))
-		q = append(q, qm.OrderBy("book_stats.month_download_count DESC"))
-	case "LOWEST_PRICE":
-		q = append(q, qm.OrderBy("book.price ASC"))
-	case "MOST_COMMENTED":
-		q = append(q, qm.InnerJoin("comment ON comment.book_id = book.id"))
-		q = append(q, qm.Where("comment.publish = 1"))
-		q = append(q, qm.OrderBy("COUNT(comment.id) DESC"))
 	}
 
 	return q
